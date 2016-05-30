0a083a0cc108b7f866a74c20b8496acd8e3990e1
Chris Riddoch <riddochc@gmail.com>
2016-05-30 14:58:44 -0600

    Making testing a bit easier for myself.

--- lib/riemann-ruby-experiments.rb
+++ lib/riemann-ruby-experiments.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+#
 require "protocol_buffers"
 require "net/tcp_client"
 require "English"
@@ -10,3 +12,9 @@ require_relative 'riemann-ruby-experiments/riemann.pb'
 require_relative 'riemann-ruby-experiments/event.rb'
 require_relative 'riemann-ruby-experiments/main'
 
+# I do a lot of testing with pry.
+if $0 == __FILE__
+  require 'pry'
+  c = Riemann::Experiment::Client.new()
+  binding.pry
+end
